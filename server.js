7a634d2453dcc4e5d12059ce1c0e2a95074fafec
NicholasLauer <lauern@matc.edu>
2025-09-02 01:00:00 -0500

    Set up project

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+//Initialize express application
+const app = express();
+
+//API endpoints
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')    
+);
+
+//Connection Listener
+app.listen(3000, () => console.log('Express Server running on port 3000'));
+

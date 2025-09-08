76757419668409917b07b7531576acbbe5b501ed
NicholasLauer <lauern@matc.edu>
2025-09-08 00:38:37 -0500

    created User API

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const UserScheme = new mongoose.SchemaType({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('user', UserScheme);
+
+export default User;
--- server.js
+++ server.js
@@ -7,10 +7,26 @@ const app = express();
 // Connect to Database
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json({ extended: false}));
+
 //API endpoints
+/**
+ * @route GET /
+ * @desc Test Endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')    
 );
+
+/**
+ * @route Post api/users
+ * @desc Registered user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //Connection Listener
 app.listen(3000, () => console.log('Express Server running on port 3000'));

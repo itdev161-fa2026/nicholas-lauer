a79086938fc2430ac0b45889aca193d1166a1599
NicholasLauer <lauern@matc.edu>
2025-09-17 21:43:59 -0500

    corrected script error.2

--- models/User.js
+++ models/User.js
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
-const UserScheme = new mongoose.SchemaType({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-const User = mongoose.model('user', UserScheme);
+const User = mongoose.model("User", UserSchema);
 
 export default User;
